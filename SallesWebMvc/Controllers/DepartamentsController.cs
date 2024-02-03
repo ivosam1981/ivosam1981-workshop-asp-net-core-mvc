90b9839cd077b1fd76f3f79cde0bcbc4460dc182
Ivo Sam <ivgest@gmail.com>
2024-02-03 12:10:34 -0300

    primeiro model-controller-view departament

---
+++ SallesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SallesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SallesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Eletronics" });
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+
+
+            return View(list);
+        }
+    }
+}
--- SallesWebMvc/Controllers/HomeController.cs
+++ SallesWebMvc/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SallesWebMvc.Models;
+using SallesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
