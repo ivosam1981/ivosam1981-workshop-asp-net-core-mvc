a0226e7342c405e4666b6cd6279d8d933f7f554a
Ivo Sam <ivgest@gmail.com>
2024-02-04 11:55:15 -0300

    delete seller

--- SallesWebMvc/Services/SellerService.cs
+++ SallesWebMvc/Services/SellerService.cs
@@ -21,11 +21,23 @@ namespace SallesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        public Seller FindById(int id)
+        {
+           return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
         public void Insert(Seller obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        } 
 
     }
 }
