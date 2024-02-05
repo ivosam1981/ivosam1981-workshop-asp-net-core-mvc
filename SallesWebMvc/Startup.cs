73f47bcbd125945a651b440214fc9835b410745b
Ivo Sam <ivgest@gmail.com>
2024-02-05 11:33:39 -0300

    app location ptBr e formatação de dados

--- SallesWebMvc/Startup.cs
+++ SallesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SallesWebMvc.Data;
 using SallesWebMvc.Services;
+using System.Globalization;
 
 namespace SallesWebMvc
 {
@@ -41,6 +42,21 @@ namespace SallesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
+
+
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
